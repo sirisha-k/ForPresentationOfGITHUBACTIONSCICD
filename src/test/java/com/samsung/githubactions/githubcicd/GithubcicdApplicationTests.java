c49a02881f8496237ad59acd90162736e2af0027
sirisha_k <s.koya@samsung.com>
2022-11-09 15:30:16 -0500

    initial code commit

---
+++ src/test/java/com/samsung/githubactions/githubcicd/GithubcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsung.githubactions.githubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
